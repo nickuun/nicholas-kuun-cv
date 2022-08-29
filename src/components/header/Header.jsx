c468fe852d11809f8b68f2790f1737346e102289
nickuun <nickuun@gmail.com>
2022-08-29 13:55:27 +0200

    TypedJS Animated Position Title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import Typed from "react-typed";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
@@ -9,7 +10,13 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello World, I'm</h5>
         <h1>Nicholas Kuün</h1>
-        <h5 className="text-light">Application Developer</h5>
+        <Typed
+          className="typed-text"
+          strings={["Application Developer", "Software Engineer", "Web Designer", "Programming Enthusiast"]}
+          typedSpeed={10000}
+          backSpeed={200}
+          loop
+        />
         <CTA></CTA>
         <HeaderSocials></HeaderSocials>
         <div className="me">
